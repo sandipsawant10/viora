a26edded6c51da54f4321612531fbef3b7065df3
sandipsawant10 <sandipsawant503@gmail.com>
2026-03-02 17:29:34 +0530

    feat: initialize backend with Express, MongoDB, and Socket.IO
    - Added package.json for backend dependencies and scripts
    - Implemented MongoDB connection in db.js
    - Created user and meeting models for MongoDB
    - Set up socket.io connection manager
    - Defined user routes for login, registration, and activity management
    - Configured Express server with CORS and JSON parsing

---
+++ backend/src/db/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log('MongoDB connected');
+}
+
+export default connectDB;
---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {createServer} from "http";
+import {Server} from "socket.io";
+import cors from "cors";
+import connectDB from "./db/db.js";
+import connectToSocket from "./controllers/socketManager.controller.js";
+
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({extended: true, limit: "40kb"}));
+
+const PORT = process.env.PORT || 3000;
+
+
+
+server.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
